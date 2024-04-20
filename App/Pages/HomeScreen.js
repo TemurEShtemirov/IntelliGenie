3ccff4b87332cd181aaf3d2e7d0e8c11e082cb3c
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-04-20 22:11:30 +0500

    start home page

--- App/Pages/HomeScreen.js
+++ App/Pages/HomeScreen.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import ChatFaceData from "../Services/ChatFaceData";
 
 export default function HomeScreen() {
+  const [chatFaceData, setChatFaceData] = useState();
+  const [selectedChatFaceData, setSelectedChatFaceData] = useState();
+
+  useEffect(() => {
+    setChatFaceData(ChatFaceData);
+  }, []);
+
   return (
     <View>
-      <Text>Home Page</Text>
+      <Text></Text>
     </View>
-  )
+  );
 }
